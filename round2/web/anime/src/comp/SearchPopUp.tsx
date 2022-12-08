859ddf095a7c6356ac83fabecb876e3e3ff51da2
rumbo <rumbo.9370@gmail.com>
2022-12-08 10:19:11 +0100

    update search genres

--- round2/web/anime/src/comp/SearchPopUp.tsx
+++ round2/web/anime/src/comp/SearchPopUp.tsx
@@ -1,22 +1,46 @@
-import React, { useEffect, Dispatch, SetStateAction } from "react";
+import React, { useEffect, useState, Dispatch, SetStateAction } from "react";
+import { Link } from "react-router-dom";
 
 interface SearchPopUpProps {
   isSearchPopUpOpen: boolean;
   setIsSearchPopUpOpen: Dispatch<SetStateAction<boolean>>;
+  setGenre: Dispatch<SetStateAction<string>>;
 }
 
 export default function SearchPopUp({
   setIsSearchPopUpOpen,
   isSearchPopUpOpen,
+  setGenre,
 }: SearchPopUpProps) {
+  const [data, setData] = useState<any[]>([]);
+  const [userInput, setUserInput] = useState<string>("");
   useEffect(() => {
     if (isSearchPopUpOpen) {
       document.body.classList.add("notScrollable");
     }
+    FetchData();
   }, [isSearchPopUpOpen]);
+
+  const FetchData = () => {
+    fetch(`https://gogo-anime-api-sand.vercel.app/api/anime-api/get-genres`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data.genres);
+      })
+      .catch((err) => {
+        console.log("error", err.message);
+      });
+  };
+
   const RemoveBodyStyle = () => {
     document.body.classList.remove("notScrollable");
     setIsSearchPopUpOpen(false);
+  };
+
+  const RemoveBodyStyleWhenSwitchPage = (item: any) => {
+    document.body.classList.remove("notScrollable");
+    setIsSearchPopUpOpen(false);
+    setGenre(item.value);
   };
 
   return (
@@ -28,7 +52,46 @@ export default function SearchPopUp({
             RemoveBodyStyle();
           }}></div>
         <div className='searchModalContent'>
-          <input type='text' />
+          <input
+            type='text'
+            onChange={(e) => {
+              setUserInput(e.target.value);
+            }}
+          />
+          <div className='filter'>
+            {data
+              .filter((post: any) => {
+                if (userInput === "") {
+                  return "";
+                } else if (
+                  post.value
+                    .toLowerCase()
+                    .includes(userInput?.toLowerCase() as string)
+                ) {
+                  return post;
+                }
+              })
+              .map((item: any) => {
+                if (
+                  item.genre !== "Hentai" &&
+                  item.genre !== "Yaoi" &&
+                  item.genre !== "Yuri" &&
+                  item.genre !== "Erotica"
+                ) {
+                  /* return <p key={item.value}>{item.value}</p>; */
+                  return (
+                    <Link
+                      to='/genre'
+                      key={item.genre}
+                      onClick={() => {
+                        RemoveBodyStyleWhenSwitchPage(item);
+                      }}>
+                      {item.genre}
+                    </Link>
+                  );
+                }
+              })}
+          </div>
         </div>
       </div>
     </>
