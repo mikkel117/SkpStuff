bdc785c8fc24380ecb5bbdfdc730deb22ffd7e91
rumbo <rumbo.9370@gmail.com>
2023-01-06 10:20:35 +0100

    css container units

---
+++ round2/web/container-test/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='wrapper'>
+        <div className='box1'>Lorem ipsum dolor sit amet.</div>
+        <div className='box2'>Lorem ipsum dolor sit amet.</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
