7e3af22e99206f94d761f1079b9c017b5123640d
rumbo <rumbo.9370@gmail.com>
2023-05-24 08:01:34 +0200

    update

--- round2/Rust/command_line_app/src/main.rs
+++ round2/Rust/command_line_app/src/main.rs
@@ -4,6 +4,7 @@ use clap::Parser;
 use std::fs;
 use std::io::stdin;
 use std::io::Read;
+use std::io::Write;
 use std::path::PathBuf;
 
 #[derive(Parser)]
@@ -18,7 +19,8 @@ fn main() -> std::io::Result<()> {
     println!("do you want to see all todo(1), or write a new todo(2)? ");
     stdin().read_line(&mut user_chose).expect("failed to read");
     if user_chose.trim() == "1" {
-        let foo: Vec<&str> = read(&args.path);
+        let foo: Result<Vec<String>, _> = read(&args.path);
+        println!("{:?}", foo.unwrap());
     } else if user_chose.trim() == "2" {
         write(&args.path);
     }
@@ -34,16 +36,24 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn read(args: &PathBuf) -> Vec<&str> {
-    let mut data = vec![];
-    let mut contents = fs::read_to_string(&args).expect("Failed to read from file");
-    for line in contents.lines() {
-        data.push(line);
-    }
-    return data;
+fn read(args: &PathBuf) -> std::io::Result<Vec<String>> {
+    let contents = fs::read_to_string(&args).expect("Failed to read from file");
+    let data = contents.lines().map(|s| s.to_string()).collect();
+    Ok(data)
 }
 
 fn write(args: &PathBuf) {
     let mut user_input = String::new();
     stdin().read_line(&mut user_input).expect("failed to read");
+    let user_input = user_input.trim_end();
+    let foo: Result<Vec<String>, std::io::Error> = read(&args);
+    let mut new_vec = vec![];
+    for item in foo.unwrap() {
+        new_vec.push(item);
+    }
+    new_vec.push(user_input.to_string());
+    let mut file = fs::File::open(args);
+    for item in new_vec {
+        &file.unwrap().write(item.as_bytes());
+    }
 }
