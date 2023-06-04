59b69d1809b41ff583b6c11f6cec3940b67b1427
rumbo <rumbo.9370@gmail.com>
2023-06-04 14:40:20 +0200

    update to yew

--- round2/Rust/yew-app/src/main.rs
+++ round2/Rust/yew-app/src/main.rs
@@ -5,10 +5,13 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     html! {
+        <>
         <h1>{ "hello world" }</h1>
+        <p>{"hey"}</p>
+        </>
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
