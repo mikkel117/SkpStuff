61c025e585a0a98dc37c252b379ad237aa3524f0
rumbo <rumbo.9370@gmail.com>
2023-06-13 12:02:21 +0200

    update

--- round2/Rust/svelte-rust/rust/src/lib.rs
+++ round2/Rust/svelte-rust/rust/src/lib.rs
@@ -1,16 +1,55 @@
-use wasm_bindgen::prelude::*;
+use std::fmt::Result;
 
-//#[derive(Debug)]
-pub struct TodoStruct {
-    
-} 
+use js_sys::Array;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
+
+/* #[derive(Serialize, Deserialize, Debug, Clone)]
+struct TodoStruct {
+    #[serde(rename = "userId")]
+    user_id: i32,
+    id: i32,
+    title: String,
+    completed: bool,
+} */
 
 #[wasm_bindgen(js_name = "vecTest")]
-pub fn vec_test() -> js_sys::Array {
-    let res = vec![1, 2, 3, 4, 5, 6, 7];
-    
-    res.into_iter().map(JsValue::from).collect()
+pub async fn vec_test() ->  {
+    let opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
+    let url = "https://jsonplaceholder.typicode.com/todos";
+
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    let json = JsFuture::from(resp.json()?).await?;
+
+    Ok(json)
 }
+
+/* #[wasm_bindgen(js_name = "vecTest")]
+pub  fn vec_test() -> js_sys::Array {
+    let res = vec![1, 2, 3, 4, 5, 6, 7];
+
+    res.into_iter().map(JsValue::from).collect()
+} */
+
+/* async fn fetch() -> reqwest::Result<TodoStruct> {
+    reqwest::Client::new()
+        .get("https://jsonplaceholder.typicode.com/todos")
+        .send()
+        .await?
+        .json()
+        .await
+} */
 
 #[wasm_bindgen]
 pub fn add(a: usize, b: usize) -> usize {
