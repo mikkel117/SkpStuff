ad00766b76799254bd3592166e021543643f672e
rumbo <rumbo.9370@gmail.com>
2023-06-15 09:44:01 +0200

    update

--- round2/Rust/tauri-svelte/src/routes/+page.svelte
+++ round2/Rust/tauri-svelte/src/routes/+page.svelte
@@ -1,6 +1,29 @@
 <script>
   import Greet from "../lib/Greet.svelte";
+  import { readDir, BaseDirectory } from "@tauri-apps/api/fs";
+  /* const getData = async (number) => {
+    let test = await invoke("plus", { number });
+    console.log(test);
+  }; */
+  import { onMount } from "svelte/types/runtime/internal/lifecycle";
+  let entries = null;
+  onMount(async () => {
+    entries = await readDir("users", {
+      dir: BaseDirectory.AppData,
+      recursive: true,
+    });
+  });
+
+  /* const processEntries = (entries) => {
+    for (const entry of entries) {
+      console.log(`Entry: ${entry.path}`);
+      if (entry.children) {
+        processEntries(entry.children);
+      }
+    }
+  }; */
 </script>
 
 <h1>Welcome to SvelteKit</h1>
+<button on:click={processEntries}>click me</button>
 <Greet />
