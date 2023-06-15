9835b24de89e33b00949dae56e40b25402e5e072
rumbo <rumbo.9370@gmail.com>
2023-06-15 14:27:43 +0200

    getting home dir and a test folder and navigate

--- round2/Rust/fileExplorer/src-tauri/src/main.rs
+++ round2/Rust/fileExplorer/src-tauri/src/main.rs
@@ -1,9 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use dirs;
+use std::{fs, io};
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, foo_bar])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -11,4 +14,26 @@ fn main() {
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
+}
+//#[tauri::command]
+
+fn foo(path: std::path::PathBuf) {
+    println!("{}", format!("{}\\test", path.display()));
+    // let updated_path = path.join("test");
+    let entries = fs::read_dir(format!("{}\\test", path.display()))
+        .unwrap()
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>();
+    println!("{:#?}", &entries);
+}
+
+#[tauri::command]
+fn foo_bar() {
+    match dirs::home_dir() {
+        Some(path) => {
+            println!("path is {}", path.display());
+            foo(path)
+        }
+        None => println!("error"),
+    }
 }
--- round2/Rust/fileExplorer/src/routes/+page.svelte
+++ round2/Rust/fileExplorer/src/routes/+page.svelte
@@ -1,6 +1,26 @@
-<script>
-  import Greet from "../lib/Greet.svelte";
+<script lang="ts">
+  import { homeDir } from "@tauri-apps/api/path";
+  import { invoke } from "@tauri-apps/api/tauri";
+  import { onMount } from "svelte";
+  /* onMount(async () => {
+    const homeDirPath = await homeDir();
+    await invoke("foo_bar", { homeDirPath });
+  }); */
+
+  async function test() {
+    /*     const homeDirPath = await homeDir();
+    console.log(homeDirPath.toString());
+
+    await invoke("foo_bar", { homeDirPath }); */
+    await invoke("foo_bar");
+  }
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<Greet />
+<button
+  on:click={() => {
+    test();
+  }}>click me</button
+>
+
+<a href="/about">about</a>
